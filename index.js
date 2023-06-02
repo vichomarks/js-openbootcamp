a82ebda94695e7d20ce582b32211344dbb4a5b36
vichomarks <bioetario@gmail.com>
2023-06-01 20:34:57 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const nombre = "Tu nombre";
+
+const edad = 30;
+
+const esDesarrollador = true;
+
+const fechaNacimiento = new Date("1990-01-01");
+
+const libroFavorito = {
+  titulo: "El libro favorito",
+  autor: "Autor del libro",
+  fecha: new Date("2022-01-01"),
+  url: "https://ejemplo.com/libro-favorito"
+};
+
+
+module.exports = {
+  nombre,
+  edad,
+  esDesarrollador,
+  fechaNacimiento,
+  libroFavorito
+};
